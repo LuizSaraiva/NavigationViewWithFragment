f0dfe0a374ac9b986830c1e39766255a80fbb97d
luiz henrique <l.henrique.saraiva@gmail.com>
2021-04-24 14:20:39 -0300

    added id without menus and extending NavigationView Listener

--- app/src/main/java/com/project/navigationviewwithfragment/MainActivity.kt
+++ app/src/main/java/com/project/navigationviewwithfragment/MainActivity.kt
@@ -2,10 +2,27 @@ package com.project.navigationviewwithfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_main)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+            R.id.item_file ->{}
+            R.id.item_shared ->{}
+            R.id.item_starred ->{}
+            R.id.item_recent ->{}
+            R.id.item_offline ->{}
+            R.id.item_upload ->{}
+            R.id.item_backup ->{}
+        }
+        return true
     }
 }
